8611b70b76b376e1ad70158c900df8b524a23215
Koster2411 <111485774+Koster2411@users.noreply.github.com>
2023-03-01 18:31:11 -0800

    good good

--- _02_variables_practice/circle_area_calculator.py
+++ _02_variables_practice/circle_area_calculator.py
@@ -12,23 +12,25 @@ if __name__ == '__main__':
     window.withdraw()
     
     # Ask the user for the radius in pixels and store it in a variable
+    radius = simpledialog.askinteger(title=("Radius"), prompt=("type a radius into this box"))
     # simpledialog.askinteger()
     
     # Make a new turtle
-    
+    turtle_ = turtle.Turtle()
     # Have your turtle draw a circle with the correct radius
-    # my_turtle.circle()
+    turtle_.circle(radius)
 
     # Call the turtle .penup() method
-
+    turtle_.penup()
     # Move your turtle to a new x,y position using .goto()
-
+    turtle_.goto(0, -60)
     # Calculate the area of your circle and store it in a variable
     # Hint, you can use math.pi
-    
+    area = math.pi*math.pow(radius,2)
     # Write the area of your circle using the turtle .write() method
-    # my_turtle.write(arg="area = " + str(area), move=True, align='left', font=('Arial',8,'normal'))
+    turtle_.write(arg="area = " + str(area), move=True, align='left', font=('Arial',20,'normal'))
 
     # Hide your turtle
-
+    turtle_.hideturtle()
     # Call turtle.done()
+    turtle.done()
---
+++ _03_if_else/_1_unbirthday/unbirthday.py
@@ -0,0 +1,7 @@
+
+import datetime
+from tkinter import messagebox, simpledialog, Tk
+window = Tk()
+window.withdraw()
+birthday = simpledialog.askinteger(title=("birthday"), prompt=("type in your birthday, month first and then day"))
+if birthday = datetime:
